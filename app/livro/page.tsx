652f30d36697f2d7ef58a4d40e42b50f7ae0457a
FernandoGabrielSilva <fernandogabrielsadasilva@gmail.com>
2024-09-13 08:17:22 -0300

    feat: search book no fão funcionado ainda

---
+++ app/livro/page.tsx
@@ -0,0 +1,38 @@
+import LivroItens from "../_components/livrosItens";
+import LivroSearch from "../_components/livrosSearch";
+import { db } from "../_lib/prisma";
+
+interface LivrosPageProps {
+  searchParams: {
+    search?: string;
+  };
+}
+
+const LivroPageSearch = async ({ searchParams }: LivrosPageProps) => {
+  const livros = await db.livros.findMany({
+    where: {
+      name: {
+        contains: searchParams?.search,
+        mode: "insensitive",
+      },
+    },
+  });
+  return (
+    <div className="m-3">
+      <div className="flex my-3 gap-2">
+        <LivroSearch />
+      </div>
+
+      <h2 className="my-3 text-gray-400">
+        Resultados para &quot;{searchParams.search}&quot;
+      </h2>
+      <div className="grid grid-cols-2 gap-2">
+        {livros.map((livros) => (
+          <LivroItens key={livros.id} livros={livros} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LivroPageSearch;
