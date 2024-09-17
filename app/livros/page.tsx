5c3eda7c48020ea427ddf80509600bc1fd766141
FernandoGabrielSilva <fernandogabrielsadasilva@gmail.com>
2024-09-17 10:44:23 -0300

    feat: update design

--- app/livros/page.tsx
+++ app/livros/page.tsx
@@ -10,7 +10,7 @@ const Livros = async () => {
       <div className="flex p-2 mt-2 gap-2">
         <LivroSearch />
       </div>
-      <div className="grid grid-cols-2 m-3 col-span-2 gap-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8">
+      <div className="grid grid-cols-2 m-3 col-span-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
         {livros.map((livros) => (
           <LivrosItens key={livros.id} livros={livros} />
         ))}
