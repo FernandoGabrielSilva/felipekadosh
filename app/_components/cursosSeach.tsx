ae18cf0c2d6f494f9b3c5dbb90324f83c8387e25
FernandoGabrielSilva <fernandogabrielsadasilva@gmail.com>
2024-09-18 08:55:37 -0300

    bug: agora pode pesquisar apenas apertando enter

--- app/_components/cursosSeach.tsx
+++ app/_components/cursosSeach.tsx
@@ -13,6 +13,19 @@ const CursoSearch = () => {
     router.push(`/curso?search=${search}`);
   };
 
+  document.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      const element: HTMLElement = document.getElementsByClassName(
+        "button"
+      )[0] as HTMLElement;
+      element.click();
+
+      if (element instanceof HTMLElement) {
+        element.click();
+      }
+    }
+  });
+
   return (
     <>
       <Input
@@ -22,7 +35,7 @@ const CursoSearch = () => {
         onChange={(e) => setSearch(e.target.value)}
       />
       <Button
-        className="bg-orange-500 text-black text-base hover:bg-transparent border-2 border-orange-500 hover:text-orange-500"
+        className="bg-orange-500 text-black text-base hover:bg-transparent border-2 border-orange-500 hover:text-orange-500 button"
         onClick={handleSubmit}
         type="submit"
       >
