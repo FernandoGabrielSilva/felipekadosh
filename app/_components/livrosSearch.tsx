ad824a3df5c89d62e5e190d9257b4afb8b53eefc
FernandoGabrielSilva <fernandogabrielsadasilva@gmail.com>
2024-09-18 08:58:35 -0300

    bug: persquisa dos livros

--- app/_components/livrosSearch.tsx
+++ app/_components/livrosSearch.tsx
@@ -13,6 +13,19 @@ const LivroSearch = () => {
     router.push(`/livro?search=${search}`);
   };
 
+  document.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      const element: HTMLElement = document.getElementsByClassName(
+        "button"
+      )[0] as HTMLElement;
+      element.click();
+
+      if (element instanceof HTMLElement) {
+        element.click();
+      }
+    }
+  });
+
   return (
     <>
       <Input
@@ -22,7 +35,7 @@ const LivroSearch = () => {
         onChange={(e) => setSearch(e.target.value)}
       />
       <Button
-        className="bg-blue-500 text-black text-base hover:bg-transparent border-2 border-blue-500 hover:text-blue-500"
+        className="bg-blue-500 text-black text-base hover:bg-transparent border-2 border-blue-500 hover:text-blue-500 button"
         onClick={handleSubmit}
         type="submit"
       >
