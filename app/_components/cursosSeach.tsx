d0fb636b8d453b23cf9bdaa7dad16e43c0d2be2f
Fernando Gabriel Silva <107934284+FernandoGabrielSilva@users.noreply.github.com>
2024-09-19 09:20:40 -0300

    Atualizar o cursosSeach.tsx

--- app/_components/cursosSeach.tsx
+++ app/_components/cursosSeach.tsx
@@ -12,14 +12,11 @@ const CursoSearch = () => {
   const handleSubmit = () => {
     router.push(`/curso?search=${search}`);
   };
-
-  const input = document.querySelector("#input");
-  input.document.addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      const btn = document.querySelector("#button").click();
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSubmit();
     }
-  });
+  };
 
   return (
     <>
@@ -28,11 +25,10 @@ const CursoSearch = () => {
         placeholder="Pesquisa..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        id="input"
+        onKeyPress={handleKeyPress}
       />
       <Button
         className="bg-orange-500 text-black text-base hover:bg-transparent border-2 border-orange-500 hover:text-orange-500"
-        id="button"
         onClick={handleSubmit}
         type="submit"
       >
