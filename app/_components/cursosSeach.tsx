fd72cc34d7ae340c679127b0d9a92bd8f5ff157a
Fernando Gabriel Silva <107934284+FernandoGabrielSilva@users.noreply.github.com>
2024-09-20 08:50:18 -0300

    Atualizar o cursosSeach.tsx

--- app/_components/cursosSeach.tsx
+++ app/_components/cursosSeach.tsx
@@ -12,7 +12,7 @@ const CursoSearch = () => {
   const handleSubmit = () => {
     router.push(`/curso?search=${search}`);
   };
-  const handleKeyPress = (e: React.ChangeEvent<any>) => {
+  const handleKeyPress = (e: React.FormEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       e.preventDefault();
       router.push(`/curso?search=${search}`);
