6ce188ef5915db296dcfa92e45e1cc9a32ddc084
FernandoGabrielSilva <fernandogabrielsadasilva@gmail.com>
2024-11-20 17:51:36 -0300

    feat: update prisma schema

---
+++ prisma/migrations/20241120205101_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "Category" AS ENUM ('Cursos', 'Livros', 'Eletronicos', 'Outros');
+
+-- CreateTable
+CREATE TABLE "Products" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "category" "Category" NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "linkUrl" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Products_pkey" PRIMARY KEY ("id")
+);
